bba20cc2f0d2ec015ac16427f74039af281b722c
susmitha sulige <135615236+susmitha-ai@users.noreply.github.com>
2025-08-16 21:35:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      const targetId = this.getAttribute("href");
+      if (targetId !== "#") {
+        e.preventDefault();
+        const target = document.querySelector(targetId);
+        if (target) {
+          target.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    });
+  });
+});
